aeca11d63a9f81f30a1ce527dcad16048d0c8f73
ybuiko <juliabuiko@gmail.com>
2023-12-15 00:23:45 -0500

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -35,6 +35,52 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
   res.send(`Tip amount: ${tip}`);
 });
 
+//************** */
+//Magic 8 Ball
+//************** */
+
+const magicBallResponse = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+  "As I see it yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
+
+// Create a route of '/magic' that should expect a phrase in the URL that ask the Magic 8 ball a question.
+
+// So if the user hits that route and asks a question of "Will I be a Millionaire" (ex. '/magic/Will%20I%20Be%20A%20Millionaire') he should have return to him his question AND a random Magic 8 ball response (see the array below) on the screen.
+// We can't use spaces in the url, so we use %20 to express a space in the url.
+// So if the user hits that route and asks a question of "Will I be a Millionaire" he should get his question asked and a random Magic 8 ball quote on the screen.
+// Send the magic 8 ball response back between html <h1> tags
+// Use this array of Magic 8 ball responses.....
+
+let number = Math.floor(Math.random() * magicBallResponse.length);
+
+app.get("/magic", (req, res) => {
+  const phrase = req.query.phrase;
+  //   const response = {phrase, magicBallResponse[numeber]}
+  res.send(
+    `<h1>${phrase}?</h1> <h2 >Magic Ball says: <span style="color:red">${magicBallResponse[number]}</span></h2>`
+  );
+});
+
+//*********************** */
 app.listen(3000, () => {
   console.log("Server is running");
 });
